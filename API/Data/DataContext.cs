2a7cdd51a08e5b391d9c1ffcf4ca53b5f1c87fa1
Cristina Panks <panksc1@outlook.com>
2021-09-11 08:44:49 -0700

    Initial Commit

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            //options are added to this constructor when we add the 
+            //dbcontext to the startup class
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
